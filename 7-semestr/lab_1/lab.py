d2cf804197d623d54fcfbe5cec665e1340143147
Vladislav Korenkov <vladnfs3@gmail.com>
2022-09-23 00:06:39 +1000

    Complete lab 1

---
+++ 7-semestr/lab_1/lab.py
@@ -0,0 +1,160 @@
+import os
+from sqlite3 import connect
+from pandas import read_sql
+from pprint import pprint
+
+DB_PATH = f'{os.path.dirname(os.path.abspath(__file__))}/mydb.sqlite3'
+
+if os.path.exists(DB_PATH):
+    os.remove(DB_PATH)
+
+connection = connect(DB_PATH)
+cursor = connection.cursor()
+
+cursor.executescript("""
+    CREATE TABLE publisher(
+        publisher_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        publisher_name VARCHAR(255)
+    );
+
+    INSERT INTO publisher (publisher_name) VALUES
+    ('ЭКСМО'),
+    ('ДРОФА'),
+    ('АСТ');
+
+    CREATE TABLE genre(
+        genre_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        genre_name VARCHAR(255)
+    );
+
+    INSERT INTO genre (genre_name) VALUES
+    ('Роман'),
+    ('Приключения'),
+    ('Детектив'),
+    ('Поэзия'),
+    ('Фантастика'),
+    ('Фэнтези');
+
+    CREATE TABLE book(
+        book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title VARCHAR(255),
+        genre_id INTEGER,
+        publisher_id INTEGER,
+        year_publication INTEGER,
+        available_numbers INTGER,
+
+        CONSTRAINT book_publisher_fk
+        FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id),
+
+        CONSTRAINT book_genre_fk
+        FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+    );
+
+    INSERT INTO book (title, genre_id, publisher_id, year_publication, available_numbers) VALUES
+    ('Мастер и Маргарита', 1, 2, 2014, 5),
+    ('Таинственный остров', 2, 2, 2015, 10),
+    ('Бородино', 4, 3, 2015, 12),
+    ('Дубровский', 1, 2, 2020, 7),
+    ('Вокруг света за 80 дней', 2, 2, 2019, 5),
+    ('Убийства по алфавиту', 1, 1, 2017, 9),
+    ('Затерянный мир', 2, 1, 2020, 3),
+    ('Герой нашего времени', 1, 3, 2017, 2),
+    ('Смерть поэта', 4, 1, 2020, 2),
+    ('Поэмы', 4, 3, 2019, 5);
+""")
+connection.commit()
+
+print("""
+1. Вывести книги (указать их жанр), количество которых принадлежит интервалу
+от a до b, включая границы (границы интервала передать в качестве параметра).
+""")
+
+cursor.execute("""
+    SELECT title, genre_name
+    FROM book
+    JOIN genre ON genre.genre_id = book.genre_id
+    WHERE available_numbers BETWEEN :min AND :max;
+""", {'min': 5, 'max': 8})
+
+pprint(cursor.fetchall())
+print('\n')
+
+print("""
+2. Вывести книги (указать их издательство), название которой состоит из одного
+слова, и книга издана после заданного года (год передать в качестве параметра).
+""")
+
+cursor.execute("""
+    SELECT title, publisher_name
+    FROM book
+    JOIN publisher ON publisher.publisher_id = book.publisher_id
+    WHERE title NOT LIKE '% %' AND year_publication > :year;
+""", {'year': 2015})
+
+pprint(cursor.fetchall())
+print('\n')
+
+print("""
+3. Вычислить, сколько экземпляров книг каждого жанра представлены в библиотеке.
+Учитывать только книги, изданные после заданного года (год передать в качестве
+параметра).
+""")
+
+cursor.execute("""
+    SELECT genre_name, Sum(available_numbers)
+    FROM book
+    JOIN genre ON genre.genre_id = book.genre_id
+    WHERE year_publication > :year
+    GROUP BY book.genre_id
+""", {'year': 2019})
+
+pprint(cursor.fetchall())
+print('\n')
+
+print("""
+Отобрать информацию о книгах, количество которых больше 3. Столбцы назвать
+Книга, Жанр, Издательство и Количество.
+Вывести отобранную информацию:
+- в виде таблицы;
+- только столбец Название;
+- 3-ю строку результата запроса;
+- количество строк и столбцов в результате запроса;
+- названия столбцов.
+""")
+
+result = read_sql("""
+    SELECT title AS Книга, genre_name AS Жанр,
+        publisher_name AS Издательство, available_numbers AS Количество
+    FROM book
+    JOIN publisher on publisher.publisher_id = book.publisher_id
+    JOIN genre on genre.genre_id = book.genre_id
+    WHERE available_numbers > 3;
+""", connection)
+
+print(result, '\n')
+print(result['Книга'], '\n')
+print(result.loc[3], '\n')
+print('Кол-во строк:', result.shape[0], '\n')
+print('Кол-во столбцов:', result.shape[1], '\n')
+print(result.dtypes.index, '\n')
+
+print("""
+1. Создать кортеж, в который включить название двух издательств. Реализовать
+запрос, который выводит книги издательств, входящих в кортеж, изданных с 2016
+по 2019 года, включая границы.
+""")
+
+genre_list = ('Поэзия', 'Роман')
+result = read_sql(f"""
+    SELECT title AS Книга, genre_name AS Жанр, publisher_name AS Издательство,
+        available_numbers AS Количество, year_publication AS Год
+    FROM book
+    JOIN publisher on publisher.publisher_id = book.publisher_id
+    JOIN genre on genre.genre_id = book.genre_id
+    WHERE genre_name in {genre_list} AND
+    year_publication BETWEEN 2016 AND 2019;
+""", connection)
+
+print(result)
+
+connection.close()
---
+++ 7-semestr/lab_1/practice.py
@@ -0,0 +1,154 @@
+import os
+from sqlite3 import connect
+from pandas import read_sql
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = f'{CWD}/library.sqlite3'
+DUMP_PATH = f'{CWD}/library.db'
+
+connection = connect(DB_PATH)
+
+with open(DUMP_PATH,'r', encoding='utf-8-sig') as f:
+    connection.executescript(f.read())
+    connection.commit()
+
+print("""
+1. Для каждого жанра посчитать, сколько различных книг этого жанра представлено в библиотеке, каково общее количество доступных экземпляров
+книг (имеющихся в наличии) и какой самый ранний год издания книг,
+относящихся к этому жанру. Информацию отсортировать по названию жанра в алфавитном порядке.
+""")
+
+result = read_sql("""
+    SELECT
+        genre_name AS Жанр,
+        count(book_id) AS 'Различных книг',
+        sum(available_numbers) AS 'Общее кол-во',
+        min(year_publication) AS 'Самое ранее издание'
+    FROM book
+    JOIN genre on book.genre_id = genre.genre_id
+    GROUP BY book.genre_id
+    ORDER BY genre.genre_name
+""", connection)
+
+print(result, '\n')
+
+print("""
+2. Вывести информацию о всех книгах, который сдал заданный читатель. Для каждой
+книги указать дату выдачи, дату сдачи и сколько дней книга была на руках.
+Информацию отсортировать по убыванию количества дней (В SQLite нет функции
+DATEDIFF, самостоятельно найти способ реализации этой функции).
+""")
+
+result = read_sql("""
+    SELECT
+        title AS Книга,
+        borrow_date AS 'Дата выдачи',
+        return_date AS 'Дата сдачи',
+        cast(julianday(return_date) - julianday(borrow_date) AS INTEGER)
+            AS 'Дней на руках'
+    FROM book
+    JOIN book_reader ON book_reader.book_id = book.book_id
+    JOIN reader ON reader.reader_id = book_reader.reader_id
+    WHERE reader_name = :reader_name
+    ORDER BY 'Была на руках' DESC
+""", connection, params={'reader_name': 'Абрамова А.А.'})
+
+print(result, '\n')
+
+print("""
+3. Вывести самый популярный жанр (жанры). Самым популярным считается жанр,
+книги которого чаще всего брали читатели в библиотеке. Вывести название жанра
+(жанров) и сколько раз читатели брали книги этого жанра. Информацию
+отсортировать по названию жанров в алфавитном порядке.
+""")
+
+result = read_sql("""
+    SELECT
+        genre_name AS Жанр ,
+        max(_count) AS 'Взято, раз'
+    FROM (
+        SELECT genre_name, count(book_id) as _count
+        FROM book
+        JOIN genre ON genre.genre_id = book.genre_id
+        GROUP BY genre.genre_id
+    ) AS subquery
+    ORDER BY genre_name
+""", connection)
+
+print(result, '\n')
+
+print("""
+1. Вывести книги, которые были взяты в библиотеке в октябре месяце. Указать
+фамилии читателей, которые их взяли, а также дату, когда их взяли. Столбцы
+назвать Название, Читатель, Дата соответственно. Информацию отсортировать
+сначала по возрастанию даты, потом в алфавитном порядке по фамилиям читателей,
+и, наконец, по названиям книг тоже в алфавитном порядке.
+""")
+
+result = read_sql("""
+    SELECT
+        title AS Название,
+        reader_name AS Читатель,
+        borrow_date AS Дата
+    FROM book
+    JOIN book_reader ON book.book_id = book_reader.book_id
+    JOIN reader ON reader.reader_id = book_reader.reader_id
+    WHERE borrow_date LIKE '%-10-%'
+    ORDER BY borrow_date DESC, reader_name, title
+""", connection)
+
+print(result, '\n')
+
+print("""
+2. Для каждой книги, изданной в заданном издательстве, вывести информацию о ее
+принадлежности к группе:
+- если книга издана раньше 2014 года, вывести "III";
+- если книга издана в период с 2014 года по 2017 год, вывести "II";
+- если книга издана позже 2017 года, вывести "I".
+Для каждой книги также указать ее жанр и год издания. Столбцы назвать
+Название, Жанр, Год, Группа. Информацию отсортировать сначала по группе в
+порядке убывания, потом возрастанию года издания и, наконец, по названию в
+алфавитном порядке.
+""")
+
+result = read_sql("""
+    SELECT
+        title AS Книга,
+        genre_name AS Жанр,
+        year_publication AS Год,
+        CASE
+            WHEN year_publication < 2014 THEN 'III'
+            WHEN year_publication BETWEEN 2014 AND 2017 THEN 'II'
+            WHEN year_publication > 2017 THEN 'I'
+        END AS Группа
+    FROM book
+    JOIN publisher ON book.publisher_id = publisher.publisher_id
+    JOIN genre ON book.genre_id = genre.genre_id
+    ORDER BY Группа DESC, year_publication, title
+""", connection, params={'publisher_name': 'ЭКСМО'})
+
+print(result, '\n')
+
+print("""
+4. Для каждой книги вывести количество экземпляров, которые есть в наличии
+(available_numbers) в библиотеке, а также сколько раз экземпляры книги брали
+читатели. Если книгу читатели не брали - вывести 0. Столбцы назвать Название,
+Количество, Количество_выдачи. Информацию отсортировать сначала по убыванию
+количества выданных экземпляров, а потом по названию книги в алфавитном порядке
+и, наконец, по возрастанию доступного количества.
+""")
+
+result = read_sql("""
+    SELECT
+        title AS Название,
+        available_numbers AS Количество,
+        count(book_reader.book_id) AS Количество_выдачи
+    FROM book
+    LEFT JOIN book_reader ON book_reader.book_id = book.book_id
+    GROUP BY book.book_id
+    ORDER BY Количество_выдачи DESC, title, available_numbers
+""", connection)
+
+print(result)
+
+connection.close()
